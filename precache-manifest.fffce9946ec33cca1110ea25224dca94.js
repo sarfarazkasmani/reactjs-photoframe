126edc14ade5e9313f814df62cc2005cebfa16c2
Sarfaraz Kasmani <sarfarazkasmani@Sarfarazs-MacBook-Air.local>
2019-06-23 20:48:07 +0530

    Updates

---
+++ precache-manifest.fffce9946ec33cca1110ea25224dca94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b949805f595176114fa5e689a2276b7",
+    "url": "/reactjs-photoframe/index.html"
+  },
+  {
+    "revision": "53bea09873f40a1a238b",
+    "url": "/reactjs-photoframe/static/css/main.a2dfde3f.chunk.css"
+  },
+  {
+    "revision": "8d22b6d479ac1f210c37",
+    "url": "/reactjs-photoframe/static/js/2.2077328c.chunk.js"
+  },
+  {
+    "revision": "53bea09873f40a1a238b",
+    "url": "/reactjs-photoframe/static/js/main.1158ee73.chunk.js"
+  },
+  {
+    "revision": "e994a16a237c0404ce99",
+    "url": "/reactjs-photoframe/static/js/runtime~main.10168820.js"
+  }
+]);
